9e89671dd9079b3abf6d1436beb9335440525ad0
Julia Vu <juliadvu@gmail.com>
2020-07-08 01:57:28 -0500

    added more comments

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -27,7 +27,7 @@ class HashTable:
         # Your code here
 
         self.capacity = [None] * MIN_CAPACITY  #initialize array with 8 empty slots for hash table
-        self.length = 0             #initialize length of the array to zero
+        self.length = 0             #initialize length of the array to zero. that acts as counter
 
 
     def get_num_slots(self):
@@ -117,13 +117,13 @@ class HashTable:
         i = self.hash_index(key)
 
         if self.capacity[i] is not None:  #if slot contains a key that is not None/empty          
-            if self.capacity[i].value is not None:      #if value is not None 
+            if self.capacity[i].value is not None:      #if value is not None. with the put we want to update the value that's why we are checking if its not None
                 current = self.capacity[i]              #set the current pointer to the first slot in the hash table
-                self.capacity[i] = HashTableEntry(key,value)  
-                self.capacity[i].next = current
+                self.capacity[i] = HashTableEntry(key,value)  #linking the class HashTableEntry and grabbing those values to link it
+                self.capacity[i].next = current     #next pointer updated to our new current value
 
                 #increment length
-                self.length +=1
+                self.length +=1                     #act as a counter to keep track of the updated value
                 
                 if self.get_load_factor() >= 0.7:           
                     self.resize(MIN_CAPACITY * 2)           #double the size of the hash table       
@@ -131,7 +131,7 @@ class HashTable:
 
         self.capacity[i] = HashTableEntry(key, value)            #making it a linked list from calling HashTableEntry class
         #increment length 
-        self.length +=1                                          # doubly linked list? 
+        self.length +=1                                       
 
         #check for load size
         if self.get_load_factor() >= 0.7:
@@ -149,7 +149,7 @@ class HashTable:
         # Your code here
 
         # self.capacity[self.hash_index(key)] = None     
-        #   
+        
         #removing that value from key-value pair and set it to None
 
         #calling put method to grab the key, and the value be changed to None    
@@ -190,21 +190,24 @@ class HashTable:
         Implement this.
         """
         # Your code here
-        global MIN_CAPACITY
+        global MIN_CAPACITY     #global keyword which allows to alter our global variable MIN_CAPACITY that was declared at the start
         MIN_CAPACITY = new_capacity         #reassign MIN_CAPACITY to new_capacity 
 
-        current = self.capacity             #set current pointer
+        current = self.capacity             #current variable for hash table
 
         self.capacity = [None] * MIN_CAPACITY       #initialize new hash table to be resized with 8 empty slots declared earlier 
 
-        for x in current:
-            if (x is not None):
+        for x in current:       #for x in current hash table
+            if (x is not None):     #if x element is not None and the x.next element is not None
                 if (x.next is not None):
-                        pointer = x.next
-                        while pointer is not None:
+                     #have pointer point to next element in the list
+                        pointer = x.next    
+                         #pointer is on the new next element on the list, and is not None
+                        while pointer is not None: 
+                            # have it updated to the new key-value pair in the slot into the current/new hash table
                             self.put(pointer.key, pointer.value)
-                            pointer = x.nest
-                self.put(x.key, x.value)
+                            pointer = x.next #have pointer be assigned to the next node in the list
+                self.put(x.key, x.value) #otherwise if there is no next node in the list, just update the key and value at the current slot
 
 
 

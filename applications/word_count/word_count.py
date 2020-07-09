289744763a411dbc2cddc1f7dc14febf2e0086f0
Julia Vu <juliadvu@gmail.com>
2020-07-08 22:53:32 -0500

    word count tests pass and added comments

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,77 @@
+"""
+input- takes a single string
+        -ignore certain characters 
+
+output- returns a dictionary of words and their counts
+        -keys all lowercased 
+        -split the strings into words on any whitespace
+       
+If the input contains no ignored characters, return an empty dictionary.
+"""
+
+
+#create an array to ignore certain characters in the string
+ignore = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+
 def word_count(s):
-    # Your code here
+    #counts dictionary for updated string
+    counts = {}
+    #loop through string/word that will be updated
+    for word in s.split():
+        #empty string to hold the updated word
+        new_word = ""
+        #check if word has characters excluding the ones in the ignore array. And add that to the new_word string
+        for character in word:
+            if character not in ignore:
+                new_word += character
+        word = new_word.lower() #lowercase the new_word string
+
+        #update the counts dictionary with the updated word
+        if word in counts:
+            counts[word] += 1
+        #else if there is no word, just an empty string break/stop running the method
+        elif word == "" or word == " ":
+            break
+        #else if there is already a word in counts have that value set to 1 
+        else:
+            counts[word] = 1
+
+    #if the string is empty then return an empty dictionary
+    if s == "":
+        return {}
+    #else string is not empty, return the counts array with the updated words    
+    else:
+        return counts
+
+
+
+# def word_count(s):
+#     # Your code here
+#     ignored = ['"', ':', ';', ',', '.', '-', '+', '=', '/', '\\', '|', '[', ']', '{', '}', '(', ')', '*', '^', '&']
+
+#     #create a dictionary for word count to be returned
+#     counts = {}
+
+    
+#     for word in s.split():
+#         new_word = ""
+#         for character in word:
+#             if character not in ignored:
+#                 new_word +=character
+
+#         word = new_word.lower()
+       
+#         if word in counts:
+#             counts[word] +=1
+#         elif word == "" or word == " ":
+#             break
+#         else:
+#             counts[word] = 1
+        
+#         if s == "":
+#             return {}
+#         else:
+#             return counts
 
 
 
